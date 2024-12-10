15086de7aabf7493a9e7b92f06b825244fbc7555
ArnaudHalvick <arnaud.halvick@gmail.com>
2024-12-10 15:32:04 -0300

    Add food images, foodService

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FoodService } from '../services/food/food.service';
 
 @Component({
   selector: 'app-home',
   imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  foods: string[] = [];
 
+  constructor(private foodService: FoodService) {}
+
+  ngOnInit(): void {
+    this.foods = this.foodService.getAll();
+  }
 }
---
+++ src/app/services/food/food.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FoodService {
+  constructor() {}
+
+  getAll(): string[] {
+    const images: string[] = [];
+    for (let i = 1; i <= 6; i++) {
+      images.push(`/assets/images/foods/food-${i}.jpg`);
+    }
+    return images;
+  }
+}
