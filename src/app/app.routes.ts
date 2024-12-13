0dff8375d4094fac812e1a9072ed71b3e8d9fbe8
ArnaudHalvick <arnaud.halvick@gmail.com>
2024-12-13 16:46:39 -0300

    Add auth component with CSS

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { FoodPageComponent } from './food-page/food-page.component';
 import { CartComponent } from './cart/cart.component';
+import { AuthComponent } from './auth/auth.component';
 
 export const routes: Routes = [
   {
@@ -24,5 +25,9 @@ export const routes: Routes = [
   {
     path: 'cart',
     component: CartComponent,
+  },
+  {
+    path: 'auth',
+    component: AuthComponent,
   },
 ];
