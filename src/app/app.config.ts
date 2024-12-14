989f59b590614e8fa0812ca92a5021b0d87c82bc
ArnaudHalvick <arnaud.halvick@gmail.com>
2024-12-14 14:11:20 -0300

    Brought app.config.ts back after fixes

---
+++ src/app/app.config.ts
@@ -0,0 +1,19 @@
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { environment } from '../environments/environment';
+
+const firebaseConfig = environment.firebase;
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(),
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideAuth(() => getAuth()),
+  ],
+};
